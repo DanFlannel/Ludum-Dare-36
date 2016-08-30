c4dd947acfb71b70cc17f960bb15edc1ad05c3b6
Dan Flanagan <dan.flanagan93@gmail.com>
2016-08-29 21:22:24 -0400

    more scene edits

---
+++ Assets/MyScripts/GUI/MultiPlayerLobbyGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MultiPlayerLobbyGUI : MonoBehaviour {
+
+    public int menuBuildIndex;
+    public GameObject lobbyPanel;
+
+    public void loadMenu()
+    {
+        Destroy(lobbyPanel);
+        SceneManager.LoadScene(menuBuildIndex);
+    }
+}
